1ff61e62dd50eb78d6a591ce55cb42fff73e0631
alejoevilches <alejoevilches@gmail.com>
2023-09-16 19:30:57 -0300

    add navbar and main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import "./Main.css"
+
+export function Main(){
+    return (
+        <main>
+            <div className="mainMessage">
+                <h1>Bienvenido a la central de reclamos de nuestra Secretaría</h1>
+                <h3>Podes iniciar reclamos y sugerencias para cualquiera de nuestras areas</h3>
+                <a href="">Iniciar reclamo</a>
+            </div>
+        </main>
+    )
+}
